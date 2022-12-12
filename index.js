fd6f3b05ad434bea1f5f8734818eaa5a593ec6a6
Matthew <turbodieseldodge@gmail.com>
2022-12-12 17:59:06 -0600

    commit tree d56c38da36ead454327dca322de5e38d7f058f9b

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    const scuberHeadquarters = 42
+    if (scuberHeadquarters - pickupLocation > 0) {
+        return scuberHeadquarters - pickupLocation
+    } else {
+        return (scuberHeadquarters - pickupLocation) * -1
+    }
+}
+
+
+
+
+function distanceFromHqInFeet(pickupLocation) {
+    const distance = distanceFromHqInBlocks(pickupLocation)
+    return distance * 264
+}
+
+
+
+
+function distanceTravelledInFeet(pickupLocation, dropOffLocation) {
+    if (dropOffLocation - pickupLocation > 0) {
+        return (dropOffLocation - pickupLocation) * 264
+    } else {
+        return (dropOffLocation - pickupLocation) * -264
+    }
+}
+
+
+function calculatesFarePrice(pickupLocation, dropOffLocation) {
+    const distance = distanceTravelledInFeet(pickupLocation, dropOffLocation)
+    const shortDistance = 400
+    const mediumDistance = 2000
+    const longDistance = 2500
+    const rate = parseFloat(.02).toFixed(2)
+    if (distance <= shortDistance) {
+        return 0
+    } else if (distance > shortDistance && distance <= mediumDistance) {
+        return (distance - shortDistance) * rate
+    } else if (distance > mediumDistance && distance <= longDistance){
+        return 25
+    } else if (distance > longDistance){
+        return 'cannot travel that far'
+    }
+}
+
+
+
+// function calculatesFarePrice(distance) {
+//     } else if (distance > shortDistance) {
+//         return (distance * num1)
+//     } else if (distance > mediumDistance) {
+//         return '25 dollars'
+//     } else if (distance > longDistance) {
+//         return 'cannot travel that far'
+//     }
